cf69ac2b80ce677489a874d18bf07ebffece0b78
Aaron Cook <aarongregorycook@gmail.com>
2022-02-11 00:39:51 -0600

    Initial dump

---
+++ tools/gen_db.py
@@ -0,0 +1,136 @@
+import json
+from typing import List, Dict, Optional
+
+def main():
+    # acquire this file from https://scryfall.com/docs/api/bulk-data
+    fd = open('default-cards.json', 'r')
+    cards: List[dict] = json.load(fd)
+    fd.close()
+
+    cards_all: List[dict] = []
+    cards_db: Dict[dict] = {}
+
+    # First, just get every card we could possibly care about
+    for card in cards:
+        # filter cards that are banned in any format
+        legalities: Dict[str, str] = card['legalities']
+        if (
+            legalities['vintage'] in [ 'not_legal', 'banned', 'restricted' ] or
+            legalities['legacy'] == 'banned' or
+            legalities['modern'] == 'banned' or
+            legalities['pioneer'] == 'banned' or
+            legalities['pauper'] == 'banned'
+        ):
+            continue
+        # only english cards
+        if card['lang'] != 'en':
+            continue
+        # no reserved list
+        if card['reserved'] is True:
+            continue
+        # paper only
+        if card['digital'] is True:
+            continue
+        # no basic lands
+        if card['name'] in [
+            'Swamp',
+            'Island',
+            'Mountain',
+            'Forest',
+            'Plains',
+            'Snow-Covered Swamp',
+            'Snow-Covered Island',
+            'Snow-Covered Mountain',
+            'Snow-Covered Forest',
+            'Snow-Covered Plains',
+            'Wastes'
+        ]:
+            continue
+        # no gold borders:
+        if card['border_color'] == 'gold':
+            continue
+
+
+
+        # get key information
+        new_card = {}
+
+        new_card['name'] = card['name']
+        new_card['set'] = card['set']
+
+        # do not consider cards without pricings
+        price = card['prices']['usd']
+        if price is None:
+            price = card['prices']['usd_foil']
+            if price is None:
+                continue
+        new_card['price'] = float(price)
+
+        rarity = card['rarity']
+        if rarity == 'common':
+            rarity = 0
+        elif rarity == 'uncommon':
+            rarity = 1
+        elif rarity == 'rare':
+            rarity = 2
+        else:
+            rarity = 3
+        new_card['points'] = rarity
+
+        new_card['uri'] = card['scryfall_uri']
+
+        # add this to the list, prices and rarity get sorted out later
+        cards_all.append(new_card)
+
+
+
+    # Then, create a unique dictionary of cards with lowest price, rarity, and all
+    # possible sets
+    for card in cards_all:
+        if cards_db.get(card['name']) is None:
+            cards_db[card['name']] = {
+                'points': card['points'],
+                'price': card['price'],
+                'sets': { card['set']: card['uri'] }
+            }
+            continue
+
+        db_card = cards_db[card['name']]
+
+        if card['price'] < db_card['price']:
+            db_card['price'] = card['price']
+        if card['points'] < db_card['points']:
+            db_card['points'] = card['points']
+        if card['set'] not in db_card['sets']:
+            db_card['sets'][card['set']] = card['uri']
+
+    # filter out cards that do not meet the price threshold (and sort by name)
+    names = sorted(list(cards_db.keys()))
+    for name in names:
+        card = cards_db[name]
+        points = card['points']
+        price = card['price']
+
+        if points == 3 and price > 2.25:
+            cards_db.pop(name)
+        elif points == 2 and price > 1.50:
+            cards_db.pop(name)
+        elif points == 1 and price > 0.75:
+            cards_db.pop(name)
+        elif points == 0 and price > 0.25:
+            cards_db.pop(name)
+
+    # output in slightly special way
+    card_lines = []
+    names = sorted(list(cards_db.keys()))
+    for name in names:
+        name_esc = name.replace('"', '\\"')
+        card_lines.append(f'  "{name_esc}": ' + json.dumps(cards_db[name]))
+    fd = open('../website/media/jank.json', 'w')
+    fd.write('{\n')
+    fd.write(',\n'.join(card_lines))
+    fd.write('\n}')
+    fd.close()
+
+if __name__ == '__main__':
+    main()
